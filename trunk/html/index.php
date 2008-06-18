0f1bb74511c09f12d90deb6524629361eb4ffa3d
wavesoft <wavesoft@2d8e69cb-894f-0410-b9a1-fb4c8e5b4f6f>
2008-06-18 19:08:46 +0000

    - Added Licence Text

--- trunk/html/index.php
+++ trunk/html/index.php
@@ -1,4 +1,21 @@
 <?php
+/*
+    GloryLands, a Web-Based, Massive Multiplayer Online RPG/Strategy Game
+    Copyright (C) 2008  John Haralampidis <johnys2@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 // Require configuration
 require_once "config/config.php";
@@ -14,4 +31,5 @@ if (( (substr($_SERVER['REQUEST_URI'],-9) == 'index.php') ||
 
 // Process the event
 require_once "engine/eventprocess.php";
+
 ?>
