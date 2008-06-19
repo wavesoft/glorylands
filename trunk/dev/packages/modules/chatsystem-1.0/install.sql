9583fded2f7c7cfd33ce2d4aa9da2e5b81c72a64
wavesoft <wavesoft@2d8e69cb-894f-0410-b9a1-fb4c8e5b4f6f>
2008-06-19 23:28:38 +0000

    - Added latest working SQL - Added chatsystem source

---
+++ trunk/dev/packages/modules/chatsystem-1.0/install.sql
@@ -0,0 +1,14 @@
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for mod_chat_channel_registrations
+-- ----------------------------
+CREATE TABLE `mod_chat_channel_registrations` (
+  `index` int(11) NOT NULL auto_increment,
+  `user` int(11) default NULL,
+  `channel` varchar(30) collate latin1_general_ci default NULL,
+  PRIMARY KEY  (`index`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+INSERT INTO `interface_module_resources` (`module`, `mode`, `filename`) VALUES ('CHATWIN', 'JS', '{DATA.MODULE}/mod_chat/chatapi.js');
+INSERT INTO `interface_modules` (`index`, `name`, `description`, `filename`) VALUES ('CHATWIN' ,'Chat Window', 'Cross-player chat window and system message receiver for any User Interface', 'chat');
+INSERT INTO `interface_module_assign` (`action`, `module`, `position`, `weight`) VALUES ('interface.main', 'CHATWIN', 2, 1);
