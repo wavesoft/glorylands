6de12bdc74c97d51305f1edb0438b0911a497607
wavesoft <wavesoft@2d8e69cb-894f-0410-b9a1-fb4c8e5b4f6f>
2008-06-20 10:16:20 +0000

    - Chat system module package updated

--- trunk/dev/packages/modules/chatsystem-1.0/hooks/hook-chatcommands.php
+++ trunk/dev/packages/modules/chatsystem-1.0/hooks/hook-chatcommands.php
@@ -25,18 +25,24 @@ function chat_admin(&$cmd, $parameters, &$answer) {
 		
 		$answer = "You are on ($x, $y) at map #$map";
 		
-	} elseif ($cmd == 'place') {
+	} elseif ($cmd == 'spawn') {
 
 		if (!isset($parameters[0])) { $answer='';
-			relayMessage(MSG_INTERFACE, 'CHAT', '<font color="#FF0000">Please, specify a guid!</font>', '<font color="gold">admin</font>');
+			relayMessage(MSG_INTERFACE, 'CHAT', '<font color="#FF0000">Please, specify a template to use!</font>', '<font color="gold">admin</font>');
 			return false;
 		}
 		
+		/*
 		if (!isset($parameters[1])) { $answer='';
 			relayMessage(MSG_INTERFACE, 'CHAT', '<font color="#FF0000">Please, specify a model!</font>', '<font color="gold">admin</font>');
 			return false;
 		}
+		*/
 		
+		$template = $parameters[0];
+		$guid = gl_instance_object(gl_make_guid($template,false,'npc'), array("x" => $x, "y" => $y, 'map' => $map));
+
+		/*
 		$guid=$parameters[0];
 		$model=$parameters[1];
 		
@@ -47,15 +53,54 @@ function chat_admin(&$cmd, $parameters, &$answer) {
 			$answer="Object @($guid) as '$model' placed on ($x,$y,$map)";
 			gl_do('map.grid.get');
 		}
+		*/
+		if ($guid>0) {
+			$answer="Object #$template (GUID: $guid) placed on ($x,$y,$map)";
+			gl_do('map.grid.get');
+		} else {
+			$answer="Spawn error!";
+		}
+
+	} elseif ($cmd == 'instance') {
+
+		if (!isset($parameters[0])) { $answer='';
+			relayMessage(MSG_INTERFACE, 'CHAT', '<font color="#FF0000">Please, specify a template to instance!</font>', '<font color="gold">debug</font>');
+			return false;
+		}
+		
+		$additional='';
+		$parent = 0;
+		if (isset($parameters[1])) {
+			$parent=$parameters[1];
+			$additional=" and parent the guid".$parent;
+		}
+				
+		$guid = gl_instance_object(gl_make_guid($parameters[0], false, 'item'), array('parent' => $parent));
+		if ($guid) {
+			$answer="Item instanced with guid{$additional}: $guid";
+		} else {
+			$answer="Cannot instance item with template id #".$parameters[0].$additional;
+		}
 
 	} elseif ($cmd == 'logout') {
 		
 		$answer='';
-		relayMessage(MSG_INTERFACE, 'NAVIGATE', 'interface.entry');
+		gl_redirect('interface.entry', true);
 		
 	}
 	return true;	
 
 }
+
+
+function chat_notify_zidchange($zid, $map) {
+	global $sql;
+
+	// Unenroll from the last channel
+	$ans=$sql->query("DELETE FROM `mod_chat_channel_registrations` WHERE `user` = ".$_SESSION[PLAYER][GUID]." AND `channel` LIKE '#%'");
+	$ans=$sql->query("INSERT INTO `mod_chat_channel_registrations` (`user`, `channel`)  VALUES (".$_SESSION[PLAYER][GUID].", '#".$map."')");
+	relayMessage(MSG_INTERFACE,'CHAT','<font color=\"#00ff00\">You have joined chat area #'.$map.'</font>','System');
+}
+
 
 ?>
--- trunk/dev/packages/modules/chatsystem-1.0/install.sql
+++ trunk/dev/packages/modules/chatsystem-1.0/install.sql
@@ -10,5 +10,6 @@ CREATE TABLE `mod_chat_channel_registrations` (
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 INSERT INTO `interface_module_resources` (`module`, `mode`, `filename`) VALUES ('CHATWIN', 'JS', '{DATA.MODULE}/mod_chat/chatapi.js');
+INSERT INTO `interface_module_resources` (`module`, `mode`, `filename`) VALUES ('CHATWIN', 'CSS', '{DATA.MODULE}/mod_chat/styles/style.css');
 INSERT INTO `interface_modules` (`index`, `name`, `description`, `filename`) VALUES ('CHATWIN' ,'Chat Window', 'Cross-player chat window and system message receiver for any User Interface', 'chat');
 INSERT INTO `interface_module_assign` (`action`, `module`, `position`, `weight`) VALUES ('interface.main', 'CHATWIN', 2, 1);
