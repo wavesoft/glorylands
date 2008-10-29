f6b57c23dedf6cc0ddc9e51c65dd05142b9d5c2e
Ioannis Charalampidis <wavesoft@users.noreply.github.com>
2008-10-29 00:35:29 +0000

    - Added: Content editor interface

---
+++ html/admin/editor/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-7" />
+<title>GloryLands :: Content Editor</title>
+</head>
+<frameset rows="*" cols="200,*" framespacing="0" frameborder="no" border="0">
+  <frame src="nav.php" name="left" scrolling="No" noresize="noresize" id="left" title="left" />
+  <frame src="home.php" name="main" id="main" title="main" />
+</frameset>
+<noframes><body>
+</body>
+</noframes></html>
---
+++ html/admin/editor/nav.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-7" />
+<title>GloryLands :: Content Editor :: Navigator</title>
+<link rel="stylesheet" type="text/css" href="theme/style.css">
+</head>
+<body class="nav">
+<button class="button" style="width: 100%">
+<img src="theme/icons/db.png" align="absmiddle" /> Registration Quiz
+</button>
+<button class="button" style="width: 100%">
+<img src="theme/icons/help.png" align="absmiddle" /> Game Tips
+</button>
+<button class="button" style="width: 100%">
+<img src="theme/icons/pan.png" align="absmiddle" /> Quests
+</button>
+<button class="button" style="width: 100%">
+<img src="theme/icons/recur.png" align="absmiddle" /> Teleports
+</button>
+<button class="button" style="width: 100%">
+<img src="theme/icons/globe2.png" align="absmiddle" /> Maps
+</button>
+<button class="button" style="width: 100%">
+<img src="theme/icons/history.png" align="absmiddle" /> Spawn Objects
+</button>
+</body>
+</html>
