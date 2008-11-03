9841018a94382b1b18fe268fb9805a62c3dd98dc
Ioannis Charalampidis <wavesoft@users.noreply.github.com>
2008-11-03 23:24:15 +0000

    - Updated: SVN Decleration on version - Updated: New file database structure on template_dictionary.php

--- html/engine/data/template_dictionary.php
+++ html/engine/data/template_dictionary.php
@@ -3,10 +3,10 @@
 # Please do not edit by hand!
 global $TableInstanceFields;
 $TableInstanceFields = array(
-'char' => array('parent','account','name','x','y','map','level','inventory_bag','model','online','visible','state','HP','MP','STR','DEX','CON','INT','WIS','CHA'),
-'gameobject' => array('parent','name','x','y','z','map','level','visible','model','mixhash'),
-'item' => array('parent','item_type','item_template','item_count','item_variables','mixhash'),
-'npc' => array('x','y','map','level','model','visible','state'),
-'unit' => array('x','y','map','level')
+'char' => array('parent','account','name','x','y','map','level','inventory_bag','model','online','visible','state','HP','MP','STR','DEX','CON','INT','WIS','CHA','contributor'),
+'gameobject' => array('parent','name','x','y','z','map','level','visible','model','mixhash','contributor'),
+'item' => array('parent','item_type','item_template','item_count','item_variables','mixhash','contributor'),
+'npc' => array('x','y','map','level','model','visible','state','contributor'),
+'unit' => array('x','y','map','level','contributor')
 );
 ?>
