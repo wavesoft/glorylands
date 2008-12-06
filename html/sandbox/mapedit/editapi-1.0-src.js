ce999e84901cb85fbbf1474cc056f68213b84459
Ioannis Charalampidis <wavesoft@users.noreply.github.com>
2008-12-06 02:41:47 +0000

    - Updated Map editor on sandbox

--- html/sandbox/mapedit/editapi-1.0-src.js
+++ html/sandbox/mapedit/editapi-1.0-src.js
@@ -4,12 +4,35 @@ var isMozilla=(navigator.userAgent.indexOf("Gecko")>=0);
 var isOpera=(navigator.userAgent.indexOf("Opera")>=0);
 
 // JavaScript Document
-function $trace(obj) {
+function $trace(obj,short) {
+
+	var ident = function(v) {
+		var parts = new String(v);
+		var ans = '';
+		parts = parts.split("\n");
+		$each(parts, function(e){
+			ans+='	'+e+'\n';
+		});
+		return ans;
+	}
+	
 	ans='';
 	$each(obj, function(value, name) {
-		if (ans!='') ans+=', ';
-		ans+='['+name+'] = '+value;
+		if (ans!='') ans+=",\n ";
+		ans+='['+name+'] = ';
+		if (!short) {
+			if (typeof(value)=='object') {
+				ans+=" {\n"+ident($trace(value))+"\n}";
+			} else if (typeof(value)=='array') {
+				ans+=" [\n"+ident($trace(value))+"\n]";
+			} else {
+				ans+=value;
+			}
+		} else {
+			ans+=value;
+		}
 	});	
+	
 	return ans;
 }
 
@@ -68,7 +91,9 @@ function json_save(){
 				var p = new String(coord);
 				if (p.indexOf(',')>0){
 					p = p.split(',');
-					gdata.push({'x':p[0], 'y':p[1], 's': paint_grid[grid_id][coord].src});
+					if (paint_grid[grid_id][coord]!=null) {
+						gdata.push({'x':p[0], 'y':p[1], 's': paint_grid[grid_id][coord].src});
+					}
 				}
 			}
 			rqdata.push(gdata);
@@ -143,6 +168,17 @@ function ui_objects() {
 	
 }
 
+function ui_clear() {
+	$('tiles_clear').setStyles({'background-color':'#FFFFFF'});
+	$('tiles_put').setStyles({'background-color':''});
+	brush_erase=true;
+}
+
+function ui_put() {
+	$('tiles_clear').setStyles({'background-color':''});
+	$('tiles_put').setStyles({'background-color':'#FFFFFF'});
+	brush_erase=false;
+}
 
 /**
   * Dropdown menu handler
@@ -234,16 +270,23 @@ function tloader_preload(images) {
 
 function tloader_renderimages(images) {
 	$('tiles_status').setHTML('<center>Displaying...</center>');
-	$each(images, function(img){
-		tloader_spawnimage(img);
-   });
-	$('tiles_status').setStyles({'display':'none'});
-	$('tiles_set').setStyles({'display':''});
+	var cur_image=0;
+	var timer = setInterval(function() {
+		for (var i=cur_image; i<cur_image+8; i++) {
+			tloader_spawnimage(images[i]);
+		}
+		cur_image=i;
+		if (i>=images.length) {
+			$('tiles_status').setStyles({'display':'none'});
+			$('tiles_set').setStyles({'display':''});
+			clearInterval(timer);
+		}
+	},10);
 }
 
 function tloader_reset() {
 	$each(tiles_cache, function(e){
-		e.remove();								
+		e.remove();
 	});
 	tiles_cache=[];
 }
@@ -315,8 +358,14 @@ function paint_spawn(image,layer) {
 
 function paint_reset() {
 	$each(paint_elements, function(e){
-		e.remove();								   
-   });
+		try {
+			e.remove();								   
+		} catch(e) {
+			// If the object is removed, the instances will
+			// be removed. If this hapens, the previous object
+			// will refer to nothing and raise an exception.
+		}
+	});
 	paint_elements = [];
 	paint_grid = [[],[],[]];
 }
@@ -330,6 +379,14 @@ function paint_updateblock(x,y,image,layer) {
 		paint_grid[layer-1][id] = paint_spawn(image,layer);
 		return paint_grid[layer-1][id];
 	}
+}
+
+function paint_clear(x,y) {
+	var id=x+','+y;
+	if ($defined(paint_grid[paint_layer-1][id])) {
+		paint_grid[paint_layer-1][id].remove();
+		paint_grid[paint_layer-1][id]=null;
+	}	
 }
 
 function paint_put(x,y,image) {
@@ -349,6 +406,15 @@ var brush_position = {x:0,y:0};
 var brush_selection = {x:0,y:0,w:0,h:0};
 var brush_dragging = false;
 var brush_dragused = false;
+var brush_erase = false;
+
+function _brush_clean() {
+	for (var y=brush_selection.y; y<brush_selection.y+brush_selection.h; y++) {
+		for (var x=brush_selection.x; x<brush_selection.x+brush_selection.w; x++) {
+			paint_clear(x+scroll_offset.x,y+scroll_offset.y);
+		}
+	}
+}
 
 function _brush_blit(sel,dest) {
 	var sx=sel.x;
@@ -356,7 +422,7 @@ function _brush_blit(sel,dest) {
 	
 	for (var y=dest.y; y<dest.y+dest.h; y++) {
 		for (var x=dest.x; x<dest.x+dest.w; x++) {
-			paint_put(x,y,tiles_base+'-'+sx+'-'+sy+'.png');
+			paint_put(x+scroll_offset.x,y+scroll_offset.y,tiles_base+'-'+sx+'-'+sy+'.png');
 			sx++;
 			if (sx>=sel.x+sel.w) {
 				sx=sel.x;
@@ -370,8 +436,95 @@ function _brush_blit(sel,dest) {
 }
 
 function _brush_stretch() {
+	var dst_center = {x:brush_selection.x,y:brush_selection.y,w:brush_selection.w,h:brush_selection.h};
 	var src_center = {x:selection.x,y:selection.y,w:selection.w,h:selection.h};
-	var dst_center = {x:brush_selection.x,y:brush_selection.y,w:brush_selection.w,h:brush_selection.h};		
+	var corners = {tl:false, tr:false, bl:false, br:false};
+
+	// Add some helping coordinates
+	brush_selection.r=brush_selection.x+brush_selection.w-1;
+	brush_selection.b=brush_selection.y+brush_selection.h-1;
+	brush_selection.c={x:brush_selection.x+Math.floor(brush_selection.w/2),y:brush_selection.y+Math.floor(brush_selection.h/2)};
+	selection.r=selection.x+selection.w-1;
+	selection.b=selection.y+selection.h-1;
+	selection.c={x:selection.x+Math.floor(selection.w/2),y:selection.y+Math.floor(selection.h/2)};
+
+	if (selection.w>2) {		
+		if (selection.h>2) {
+			// * * *
+			// *   *
+			// * * *
+			
+			// 4 Corners
+			_brush_blit({x:selection.x, y:selection.y, h:1,w:1},
+						{x:brush_selection.x, y:brush_selection.y, h:1,w:1});
+			_brush_blit({x:selection.r, y:selection.y, h:1,w:1},
+						{x:brush_selection.r, y:brush_selection.y, h:1,w:1});
+			_brush_blit({x:selection.x, y:selection.b, h:1,w:1},
+						{x:brush_selection.x, y:brush_selection.b, h:1,w:1});
+			_brush_blit({x:selection.r, y:selection.b, h:1,w:1},
+						{x:brush_selection.r, y:brush_selection.b, h:1,w:1});
+
+			// 4 Sides
+			_brush_blit({x:selection.x, y:selection.y+1, h:selection.h-2,w:1},
+						{x:brush_selection.x, y:brush_selection.y+1, h:brush_selection.h-2,w:1});
+			_brush_blit({x:selection.r, y:selection.y+1, h:selection.h-2,w:1},
+						{x:brush_selection.r, y:brush_selection.y+1, h:brush_selection.h-2,w:1});
+			_brush_blit({x:selection.x+1, y:selection.y, h:1,w:selection.w-2},
+						{x:brush_selection.x+1, y:brush_selection.y, h:1,w:brush_selection.w-2});
+			_brush_blit({x:selection.x+1, y:selection.b, h:1,w:selection.w-2},
+						{x:brush_selection.x+1, y:brush_selection.b, h:1,w:brush_selection.w-2});
+			
+			// Resize center
+			src_center.x+=1;
+			src_center.y+=1;
+			src_center.w-=2;
+			src_center.h-=2;
+			dst_center.x+=1;
+			dst_center.y+=1;
+			dst_center.w-=2;
+			dst_center.h-=2;
+			
+		} else {
+			// * - *
+			// *   *
+			// * - *
+
+			// 2 Sides Left/Right
+			_brush_blit({x:selection.x, y:selection.y, h:selection.h,w:1},
+						{x:brush_selection.x, y:brush_selection.y, h:brush_selection.h,w:1});
+			_brush_blit({x:selection.r, y:selection.y, h:selection.h,w:1},
+						{x:brush_selection.r, y:brush_selection.y, h:brush_selection.h,w:1});
+
+			// Resize center
+			src_center.x+=1;
+			src_center.w-=2;
+			dst_center.x+=1;
+			dst_center.w-=2;
+		}
+	} else {
+		if (selection.h>2) {
+			// * * *
+			// |   |
+			// * * *
+			
+			// 2 Sides Up/Down
+			_brush_blit({x:selection.x, y:selection.y, h:1,w:selection.w},
+						{x:brush_selection.x, y:brush_selection.y, h:1,w:brush_selection.w});
+			_brush_blit({x:selection.x, y:selection.b, h:1,w:selection.w},
+						{x:brush_selection.x, y:brush_selection.b, h:1,w:brush_selection.w});
+			
+			// Resize center
+			src_center.y+=1;
+			src_center.h-=2;
+			dst_center.y+=1;
+			dst_center.h-=2;
+		} else {
+
+		}
+	}
+
+	// Render center
+	_brush_blit(src_center,dst_center);
 }
 
 function _brush_fill() {
@@ -390,15 +543,19 @@ function _brush_copy_once() {
 }
 
 function brush_apply(layer) {
-	if (brush_dragused) {
-		if ((brush_selection.w==1)&&(brush_selection.h==1)) {
-			// Used rectangle but canceled? Return..
-			return;
+	if (!brush_erase) {
+		if (brush_dragused) {
+			if ((brush_selection.w==1)&&(brush_selection.h==1)) {
+				// Used rectangle but canceled? Return..
+				return;
+			} else {			
+				_brush_stretch();
+			}
 		} else {
-			_brush_fill();
+			_brush_copy_once();
 		}
 	} else {
-		_brush_copy_once();
+		_brush_clean();	
 	}
 }
 
@@ -582,13 +739,61 @@ function selection_dragto(x,y) {
 	brush_updateview();
 }
 
+
+/**
+  *  Area resizing and scrolling
+  */
+
+var scroll_active = false;
+var scroll_hit = {x:0, y:0};
+var scroll_base = {x:0, y:0};
+var scroll_offset = {x:0, y:0};
+
+function scroller_setpos(x,y) {
+	if (x>0) x=0;
+	if (y>0) y=0;
+	
+	x=Math.round(x/32)*32;
+	y=Math.round(y/32)*32;
+	
+	scroll_offset.x = -Math.round(x/32);
+	scroll_offset.y = -Math.round(y/32);
+	
+	$('content_layer1').setStyles({'left':x,'top':y});
+	$('content_layer2').setStyles({'left':x,'top':y});
+	$('content_layer3').setStyles({'left':x,'top':y});
+	$('content_objects').setStyles({'left':x,'top':y});
+	$('spacer').setStyles({
+		'left':0, 'top':0, 'width':x+758, 'height':y+502
+	});
+}
+
+function scroller_start(x,y) {
+	var pos = $('content_layer1').getPosition();
+	scroll_hit.x=x;
+	scroll_hit.y=y;
+	scroll_base.x=pos.x;
+	scroll_base.y=pos.y;
+	$('msg').setHTML('Started: '+$trace(scroll_hit)+' Base:'+$trace(scroll_base));
+}
+
+function scroller_move(x,y) {
+	scroller_setpos(scroll_base.x+(x-scroll_hit.x),scroll_base.y+(y-scroll_hit.y));
+}
+
+
+/**
+================================================================================================================================
+================================================================================================================================
+**/
+
 $(window).addEvent('load', function(e){	
 
 	tloader_download('z-field-ext');
 
 	$('content_host').addEvent('mousemove', function(e){
 		var e = new Event(e);
-	
+		
 		var offset = $('content_host').getSize().scroll;
 		var position = e.client;
 		var x = e.client.x+offset.x-$('content_host').getLeft();
@@ -599,7 +804,9 @@ $(window).addEvent('load', function(e){
 		paint_x=x;
 		paint_y=y;
 		
-		if (brush_dragging) {
+		if (scroll_active) {
+			scroller_move(e.client.x,e.client.y);
+		} else if (brush_dragging) {
 			brush_selection_drag(x,y);
 		} else {
 			brush_move(x,y);
@@ -610,28 +817,37 @@ $(window).addEvent('load', function(e){
 	
 	$('content_host').addEvent('mousedown', function(e){
 		var e = new Event(e);
-		brush_dragging = true;
 		dropdown_dispose();	
+		if (e.event.button == 1) {
+			scroller_start(e.client.x,e.client.y);
+			scroll_active = true;
+		} else if (e.event.button == 0) {
+			brush_dragging = true;			
+		}
 		e.stop();
 	})
 	
 	$('content_host').addEvent('mouseup', function(e){
 		var e = new Event(e);
-		brush_dragging = false;
-		if (e.shift) {
-			paint_layer=2;
-		} else if (e.control) {
-			paint_layer=3;
-		} else {
-			paint_layer=1;
+		if (brush_dragging) {
+			if (e.shift) {
+				paint_layer=3;
+			} else if (e.control) {
+				paint_layer=1;
+			} else {
+				paint_layer=2;
+			}
+			brush_apply();
 		}
-		brush_apply();
+		brush_dragging = false;
+		scroll_active = false;
 		e.stop();
 	});
 	
 	$('tiles_host').addEvent('mouseup', function(e){
 		var e = new Event(e);
 		selection_dragging=false;
+		ui_put();
 		e.stop();
 	});
 
@@ -687,6 +903,13 @@ $(window).addEvent('load', function(e){
 		dropdown_dispose();	
 		e.stop();
 	});
+	
+	$(document).addEvent('mouseup', function(e){
+		var e = new Event(e);
+		brush_dragging = false;
+		scroll_active = false;											 
+		e.stop();
+	 });
 
 });
 
