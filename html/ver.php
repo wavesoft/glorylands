674c8fe8ca8f30dc95ca52134f1e5787fe432625
Ioannis Charalampidis <wavesoft@users.noreply.github.com>
2009-02-18 15:22:46 +0000

    - Version update

--- html/ver.php
+++ html/ver.php
@@ -3,8 +3,8 @@ global $_VER;
 
 /* ========[ ENGINE VERSION INFORMATION ]======== */
 
-$_VER['VERSION'] = '0.3b (SVN Version)';
-$_VER['REVISION'] = 87;
+$_VER['VERSION'] = '0.4b (SVN Version)';
+$_VER['REVISION'] = 112;
 $_VER['MILESTONE'] = 'Chaos';
 
 /* ====[ END OF ENGINE VERSION INFORMATION ]===== */
