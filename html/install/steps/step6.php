d7d4377d152316d2bed7994704648fbd1d0b2535
Ioannis Charalampidis <wavesoft@users.noreply.github.com>
2009-02-18 15:20:41 +0000

    - Installer is now stable

--- html/install/steps/step6.php
+++ html/install/steps/step6.php
@@ -1,6 +1,3 @@
-<?php
-session_destroy();
-?>
 <h1>Installation is now completed</h1>
 <p>You can now start using the game!</p>
 <p>Log into the game from the <a href="..">Main Page</a> using the username <strong>admin</strong> and password <strong>admin</strong>, or create a new user account!</p>
@@ -13,7 +10,11 @@ session_destroy();
 </p>
 <p>If you want, you can let the installer attempt to perofrm those steps, by clicking the following button:</p>
 <form action="" method="post">
+<ul>
 <input type="hidden" name="step" value="6" />
 <input type="hidden" name="prev_step" value="6" />
-<input type="submit" class="button" value="Perform Finalizations" />
+<li><input checked="checked" type="checkbox" name="finalize[sql]" value="true" id="fin_sql" /><label for="fin_sql"> Change the MySQL user permissions to SELECT, INSERT, UPDATE, DELETE only</label></li>
+<li><input checked="checked" type="checkbox" name="finalize[files]" value="true" id="fin_files" /><label for="fin_files"> Make the configuration file non-writable</label></li>
+</ul>
+<p><input type="submit" class="button" value="Perform Finalizations" /></p>
 </form>
